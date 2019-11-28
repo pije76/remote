4701274de3858e78644e35b607b3a60bcfbb6eb3
Panjianom <panjul76@hotmail.com>
2019-11-29 00:14:44 +0700

    allinone

--- remote/settings.py
+++ remote/settings.py
@@ -120,22 +120,16 @@ WSGI_APPLICATION = 'remote.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#        'NAME': 'remote',
-#        'USER': 'pije76',
-#        'HOST':'localhost',
-#        'PASSWORD': 'tratap60',
-#        'PORT': '3306',
-#    }
-#}
 
 import dj_database_url
 
-DATABASES['default'] = dj_database_url.config(default='mysql://pije76:tratap60@localhost:3306/remote')
+DATABASE_URL = 'mysql://pije76:tratap60@localhost:3306/remote'
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=DATABASE_URL
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
