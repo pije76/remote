fe32922415dbbab2bab92691da69e6cb21103682
Panjianom <panjul76@hotmail.com>
2019-11-28 23:11:46 +0700

    allinone

--- freelancer/admin.py
+++ freelancer/admin.py
@@ -5,8 +5,12 @@ import tagulous.admin
 from . import models
 
 # Register your models here.
+class FreelancerProfileAdmin(admin.ModelAdmin):
+    list_display = ('username', 'category', 'skill', 'rate')
+    list_filter = ('username', 'category', 'skill', 'rate')
+
 #admin.site.register(FreelancerProfile)
-tagulous.admin.register(models.)
+tagulous.admin.register(models.FreelancerProfile, FreelancerProfileAdmin)
 #admin.site.register(Skill)
 #tagulous.admin.register(Skill, SkillAdmin)
 #tagulous.admin.register(models.Skill)
